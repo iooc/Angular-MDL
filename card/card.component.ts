27b16aa491ec55184ab70866c95cf9dfcf266b8d
Cocker <outine@gmail.com>
2016-11-23 12:32:58 +0800

    card edit

--- card/card.component.ts
+++ card/card.component.ts
@@ -11,12 +11,22 @@
 import { Component, Input } from '@angular/core';
 
 /**
- * mdl card 多容器组件
+ * mdl card 卡片式多容器组件
  */
 @Component({
     selector: 'mdl-card',
-    template: '<div class="mdl-card"></div>'
+    template: `<div class="mdl-card mdl-shadow--2dp">
+                   <div class="mdl-card__title">
+                   </div>
+                   <div class="mdl-card__media">
+                       
+                   </div>
+                   <div class="mdl-card__supporting-text">
+                   </div>
+                   <div class="mdl-card__actions">
+                   </div>
+               </div>`
 })
 export class MdlCard {
-
+    constructor() { }
 }
